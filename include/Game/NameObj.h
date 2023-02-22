40fb426fcee454ba5586d4a4720716b855de16a4
Aurum <nintendo.rpgforum@gmail.com>
2023-02-22 03:13:36 +0100

    finished all NameObj headers

--- include/Game/NameObj.h
+++ include/Game/NameObj.h
@@ -6,6 +6,7 @@
 #include "Game/NameObj/NameObjGroup.h"
 #include "Game/NameObj/NameObjHolder.h"
 #include "Game/NameObj/MovementOnOffGroupHolder.h"
+#include "Game/NameObj/NameObjCategories.h"
 #include "Game/NameObj/NameObjCategoryList.h"
 #include "Game/NameObj/NameObjFactory.h"
 #include "Game/NameObj/NameObjFinder.h"
