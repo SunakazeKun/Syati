0a83e24ed633ca270bdf2a733bdcb53280969fd5
Aurum <nintendo.rpgforum@gmail.com>
2023-02-24 12:29:50 +0100

    header for HeapMemoryWatcher

--- include/JSystem/JKernel/JKRHeap.h
+++ include/JSystem/JKernel/JKRHeap.h
@@ -61,11 +61,11 @@ public:
 void JKRDefaultMemoryErrorRoutine(void *, u32, s32);
 
 void* operator new(size_t);
-void* operator new(size_t, s32);
+void* operator new(size_t, int);
 void* operator new(size_t, JKRHeap*, int);
 
 void* operator new[](size_t);
-void* operator new[](size_t, s32);
+void* operator new[](size_t, int);
 void* operator new[](size_t, JKRHeap*, int);
 
 void operator delete(void*);
