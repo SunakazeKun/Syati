8bef88295650f70b0501aa72922617ac67499898
Aurum <nintendo.rpgforum@gmail.com>
2023-04-05 15:10:26 +0200

    System symbols + GameSequenceFunction header

--- README.md
+++ README.md
@@ -2,12 +2,13 @@
 **Syati** provides C++ headers and symbols for writing custom code injections in **Super Mario Galaxy 2**. With this, you can write code, compile code, link to existing functions and structures in the game and load the actual changes. It was originally created by **shibbo**, however, the original repository has been deleted.
 
 # What is supplied
-Syati provides C++ headers, symbols, the CustomCode loader and five sample C++ code files.
+Syati provides C++ headers, symbols, the CustomCode loader and six sample C++ code files.
 
 - **loader**: The source code for the CustomCode loader and crash debugger patches. The loader automatically adjusts the game's memory heaps to fit a large batch of new code. The CustomCode binary that it loads depends on the version of the game played. If a CustomCode binary doesn't exist, the loader initialization is aborted, however, the crash debugger will still be enabled.
 - **init.cpp**: Calls all linked static initializers. This is necessary to initialize static variables and constants in the BSS segment of your code. Custom Nerve instances will be stored there, for example.
 - **ExtNameObjFactory.cpp**: Extends the existing ``NameObjFactory`` by taking into account your table of new objects. The custom creation table with actor entries is defined in the respective header file.
 - **ExtSceneChangeArea.cpp**: A reimplementation of ``SceneChangeArea``'s "control" method. It takes two arguments in, *Obj_arg0* accesses a list to which Galaxy to go to, and *Obj_arg1* determines the white screen fade time.
+- **PadRecord.cpp**: Helper code for recording PAD input playback files. Requires the external tool [galaxymsbt](https://github.com/SunakazeKun/galaxypad) to produce PAD files during gameplay.
 - **archive.cpp**: A simplified archive loader. It can be used to mount any archive from the game's filesystem and access its stored files. The files are stored in ``StationedHeapNapa``.
 
 # Future Plans
--- include/Game/System/AllData/GameSequenceFunction.h
+++ include/Game/System/AllData/GameSequenceFunction.h
@@ -1,40 +1,129 @@
 #pragma once
 
 #include "revolution.h"
+#include "JSystem.h"
 
-class JMapIdInfo;
 class GameSequenceInGame;
+class GameSequenceEnding;
+class JMapIdInfo;
+class NameObj;
 class PlayResultInStageHolder;
+class SaveDataHandleSequence;
+class UserFile;
 
 class GameSequenceFunction {
 public:
+	// -------------------------------------------------------------------------------------------------------------------
+	// (Anonymous) helper functions
+
 	static GameSequenceInGame* getGameSequenceInGame();
+	static GameSequenceEnding* getGameSequenceEnding();
+	static SaveDataHandleSequence* getSaveDataHandleSequence();
+	// getNWC24Messenger
+	static void closeWipeCircleToPlayerHead();
+	static void closeWipeCircleAndPrepareDreamer(const NameObj *pDreamer, s32 spawnId);
+	static void prepareStageResetLuigiIntrusively(bool isLuigi, const TVec3f &rWipePos);
+	static void prepareStageResetChangeRoom(s32 spawnId, bool isLuigi);
+	static bool isClearedPowerStar(const char *pStageName, s32 scenarioId);
+	static bool isClearedGrandStarVsKoopaLv3GalaxyFirstTime();
 
-	static bool isActiveSaveDataHandleSequence();
+
+	// -------------------------------------------------------------------------------------------------------------------
+	// SaveDataHandleSequence
+
+	static void startPreLoadSaveDataSequence();
 	static void startGameDataSaveSequence(bool, bool, bool);
+	// unknown functions
+	void startCreateUserFileSequence(int);
+	void startDeleteUserFileSequence(int);
+	// unknown function
+	static bool isActiveSaveDataHandleSequence();
+	static bool isSuccessSaveDataHandleSequence();
+	static void restoreUserFile(UserFile *pUserFile, int);
+	// unknown functions
 
-	static void changeSceneStageMarioFaceShipGalaxy(s32 spawnId, s32 scenario);
+
+	// -------------------------------------------------------------------------------------------------------------------
+	// NWC24
+
+	static bool tryNANDErrorSequence(s32);
+	static void requestPrepareResetNWC24();
+	static bool isEnableToResetNWC24();
+	static void resetNWC24();
+
+
+	// -------------------------------------------------------------------------------------------------------------------
+	// Scene changes
+
+	static void notifyToGameSequenceProgressToEndScene();
+	static void requestChangeStageAfterFileSelect();
+	static void requestChangeStageMarioFaceShip(s32 spawnId, s32 scenarioId);
+	static void requestChangeStageWorldMap();
+
+	static void requestChangeStageAfterMiss();
 	static void restartCurrentStageAndClearAfterMiss();
-	static void changeToScenarioSelect(const char *pStageName);
+	static void requestChangeScenarioSelect(const char *pStageName);
+	static void requestChangeStageMarioFaceShipBeforeKoopaVs3();
+	static void requestChangeStageStaffRoll();
+	static void requestChangeStageChildRoom();
 
+
+	// -------------------------------------------------------------------------------------------------------------------
+	// PlayResultInStage
+	
+	// unknown functions
+	static void setClearedPowerStarNum(s32);
 	static bool hasStageResultSequence();
-
+	// unknown functions
 	static bool isResultNotifyTicoCoin();
-
+	// unknown function
 	static const char* getClearedStageName();
 	static s32 getClearedPowerStarId();
 	static s32 getClearedStarPieceNum();
 	static s32 getClearedCoinNum();
-
+	static u32 getResultBestTime();
+	static bool isResultNewBestTime();
+	// unknown functions
+	static bool isResultUnlockGhostLuigi();
+	static bool isClearedIslandFleetGalaxyFirstTime();
+	static bool isClearedVsKoopaLv3GalaxyFirstTime();
+	static bool isClearedVsKoopaLv3Galaxy1();
 	static PlayResultInStageHolder* getPlayResultInStageHolder();
 
+	// unknown functions
+
+	static bool hasRetryGalaxySequence();
+
+	// unknown functions
+
+	static void resetPlayResultInStage();
+	
+	// unknown PlayResultWorldMapHolder functions
+
+
+	// -------------------------------------------------------------------------------------------------------------------
+	// PlayResultMarioFaceShip
+	
+	// unknown functions
 	static bool isFaceShipRespondGameOver();
 	static void setFaceShipRespondGameOver();
 	static void clearFaceShipRespondGameOver();
-
+	static bool isFaceShipSkipBankInfo();
+	static void setFaceShipSkipBankInfo();
 	static s32 getFaceShipClearedScenariosCount();
 	static void incFaceShipClearedScenariosCount();
 	static void clearFaceShipClearedScenariosCount();
+	// unknown functions
+	
+
+	// -------------------------------------------------------------------------------------------------------------------
+	// PlayResult reset
+
+	static void resetPlayResultHolders();
+	
+
+	// -------------------------------------------------------------------------------------------------------------------
+	// PAD playback
 
 	static void setPadModeDreamer();
 	static void setPadModeSuperDreamer();
@@ -47,15 +136,39 @@ public:
 	static bool isAllowDreamerMissNum(s32 missNum);
 	static bool isAllowDreamerInStage();
 
-	static void forcePlayerLuigiAndGotoMarioFaceShipGalaxy();
-	static void forcePlayerMarioAndGotoMarioFaceShipGalaxy();
+
+	// -------------------------------------------------------------------------------------------------------------------
+	// Player changing
+	
+	// unknown functions
+	static void prepareStageResetLuigiIntrusivelyLuigi(const TVec3f &rWipePos);
+	static void prepareStageResetLuigiIntrusivelyMario(const TVec3f &rWipePos);
+	static void prepareStageResetChangeRoomLuigi(s32 spawnId);
+	static void prepareStageResetChangeRoomMario(s32 spawnId);
+
+
+	// -------------------------------------------------------------------------------------------------------------------
+	// Ghost Luigi
+
+	static bool checkAppearGhostLuigi(const char *pStageName, s32 scenarioNo);
+	static s32 getPlacedGreenStarScenario(const char *pStageName, s32 greenStarNo);
+
+
+	// -------------------------------------------------------------------------------------------------------------------
+	// Comet Medal
 
 	static void collectTicoCoinInStage();
 	static bool isCollectTicoCoinInStage();
 	static bool isCollectTicoCoinSaved();
 	static void collectTicoCoinIfSaved();
 
-	static void changeSceneStage(const char *pStageName, s32 scenarioNo, s32, const JMapIdInfo &rStartIdInfo);
 
+	// -------------------------------------------------------------------------------------------------------------------
+	// Miscellaneous
+	
+	// unknown functions
+	static void requestChangeStage(const char *pStageName, s32 scenarioNo, s32, const JMapIdInfo &rStartIdInfo);
+	// unknown functions
 	static void requestChangeScene(const char *pSceneName);
+	// unknown functions
 };
