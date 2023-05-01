54ac76f701d5647d8b6ce11e2b346832bd16a789
Lord-Giganticus <70081529+Lord-Giganticus@users.noreply.github.com>
2023-05-01 11:52:15 -0600

    Document FunctorV1M and FunctorV2M, and fix alignment

--- include/Game/Util/Functor.h
+++ include/Game/Util/Functor.h
@@ -24,7 +24,7 @@ namespace MR {
         }
 
         virtual FunctorBase* clone(JKRHeap *pHeap) const {
-            return new (pHeap, 0) FunctorV0M(mCaller, mCallee);
+            return new (pHeap, 0x14) FunctorV0M(*this);
         };
 
         T mCaller;
@@ -34,6 +34,63 @@ namespace MR {
     template<class T>
     static FunctorV0M<T *, void (T::*)()> Functor(T* caller, void (T::*callee)()) {
         return FunctorV0M<T *, void (T::*)()>(caller, callee);
+    }
+
+    template<typename T, typename U, typename V>
+    class FunctorV1M : public FunctorBase {
+        public:
+        inline FunctorV1M() { }
+
+        inline FunctorV1M(T caller, U callee, V arg0) :
+        mCaller(caller), mCallee(callee), mArg0(arg0) {
+
+        }
+
+        virtual void operator()() const {
+            (mCaller->*mCallee)(mArg0);
+        }
+
+        virtual FunctorBase* clone(JKRHeap *pHeap) const {
+            return new (pHeap, 0x1C) FunctorV1M(*this);
+        };
+
+        T mCaller;
+        U mCallee;
+        V mArg0;
+    };
+
+    template<typename T, typename U>
+    static FunctorV1M<T *, void (T::*)(U), U> Functor(T* caller, void (T::*callee)(U), U arg0) {
+        return FunctorV1M<T *, void (T::*)(U), U>(caller, callee, arg0);
+    }
+
+    template<typename T, typename U, typename V, typename W>
+    class FunctorV2M : public FunctorBase {
+        public:
+        inline FunctorV2M() { }
+
+        inline FunctorV2M(T caller, U calee, V arg0, W arg1) :
+        mCaller(caller), mCallee(calee), mArg0(arg0), mArg1(arg1) {
+
+        }
+
+        virtual void operator()() const {
+            (mCaller->*mCallee)(mArg0, mArg1);
+        }
+
+        virtual FunctorBase* clone(JKRHeap *pHeap) const {
+            return new (pHeap, 0x16) FunctorV2M(*this);
+        }
+
+        T mCaller;
+        U mCallee;
+        V mArg0;
+        W mArg1;
+    };
+
+    template<class T, typename U, typename V>
+    static FunctorV2M<T *, void (T::*)(U, V), U, V> Functor(T* caller, void (T::*callee)(U, V), U arg0, V arg1) {
+        return FunctorV2M<T *, void (T::*)(U, V), U, V>(caller, callee, arg0, arg1);
     }
 
     class FunctorV0F : public FunctorBase {
