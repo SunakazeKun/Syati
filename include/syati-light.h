baecd4a62948fe9f7142ce0721af6f917196fff0
Aurum <nintendo.rpgforum@gmail.com>
2023-07-29 00:11:36 +0200

    Create syati-light.h

---
+++ include/syati-light.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifndef __MWERKS__
+#error "Kamek requires the CodeWarrior compiler!"
+#endif
+
+#include "revolution.h"
+#include "c_stdlib.h"
+#include "JSystem.h"
+#include "LMS.h"
+#include "nw4r.h"
+#include "kamek/hooks.h"
